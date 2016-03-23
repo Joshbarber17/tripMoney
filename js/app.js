3f6ed72d523e4a82c256d99f0cc0b3214eee9474
Josh Barber <Joshbarber17@me.com>
2016-03-22 20:02:25 -0600

    routes and views set up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-angular.module('tripMoney', []);
+angular.module('tripMoney', ['ui.router']).config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('landing', {
+    url: '/',
+    templateUrl: '../views/landing.html'
+  })
+  .state('trips', {
+    url: '/trips',
+    templateUrl: '../views/trips.html'
+  })
+  .state('trips.newTrip', {
+    url: '/newTrip',
+    templateUrl: '../views/newTrip.html'
+  })
+  .state('trips.currentTrip', {
+    url: '/currentTrip',
+    templateUrl: '../views/currentTrip.html'
+  })
+  .state('trips.pastTrips', {
+    url: '/pastTrips',
+    templateUrl: '../views/pastTrips.html'
+  });
+});
--- js/controllers/homeCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('tripMoney').controller('homeCtrl', function($scope){
-  $scope.test = "testing";
-});
